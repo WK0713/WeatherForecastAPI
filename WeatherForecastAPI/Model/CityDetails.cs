0121c67268a454ab79c49b523b376f6e63ac6651
WK0713 <tan_kee1020@hotmail.com>
2023-08-29 21:28:20 +0800

    Add "CityDetails.cs" file

---
+++ WeatherForecastAPI/Model/CityDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecastAPI.Model
+{
+    public class CityDetails
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
