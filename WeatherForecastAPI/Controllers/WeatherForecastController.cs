9e63ceff53f2c9d9cdf1a2f63763bd28cf1954cd
WK0713 <tan_kee1020@hotmail.com>
2023-08-29 21:48:06 +0800

    Add "WeatherForecastController.cs" file

---
+++ WeatherForecastAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
