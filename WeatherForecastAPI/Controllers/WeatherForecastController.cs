f84fec6dd168bb66065a83e0671ba4c250ddd96f
WK0713 <tan_kee1020@hotmail.com>
2023-08-29 22:47:34 +0800

    Add GetCitiesList GET method

--- WeatherForecastAPI/Controllers/WeatherForecastController.cs
+++ WeatherForecastAPI/Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WeatherForecastAPI.Controllers
 {
@@ -7,5 +9,34 @@ namespace WeatherForecastAPI.Controllers
     [ApiController]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        SqlConnection sqlConnection;
+
+        public WeatherForecastController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            sqlConnection = new SqlConnection(_configuration.GetConnectionString("WeatherForecast"));
+        }
+
+        [Route("ListOfCities")]
+        [HttpGet]
+        public JsonResult GetCitiesList()
+        {
+            string query = "SELECT City, State, Country, Latitude, Longitude FROM CityDetails";
+            DataTable table = new DataTable();
+            SqlDataReader sqlReader;
+            using (sqlConnection)
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlReader = sqlCommand.ExecuteReader();
+                    table.Load(sqlReader);
+                    sqlReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
     }
 }
