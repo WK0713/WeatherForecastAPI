b9a75afd81f31716b113189a1e4d13cedd8cb5b2
WK0713 <tan_kee1020@hotmail.com>
2023-08-31 01:12:08 +0800

    Add "DailyFeelsLike.cs" file

---
+++ WeatherForecastAPI/Model/DailyFeelsLike.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForecastAPI.Model
+{
+    public class DailyFeelsLike
+    {
+        public double morn { get; set; }
+        public double day { get; set; }
+        public double eve { get; set; }
+        public double night { get; set; }
+    }
+}
