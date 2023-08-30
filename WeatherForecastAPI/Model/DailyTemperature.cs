526cdc92fa4cd1f85fa3fa418eceee5ae08ca4d9
WK0713 <tan_kee1020@hotmail.com>
2023-08-30 16:36:32 +0800

    Add "DailyTemperature.cs" file

---
+++ WeatherForecastAPI/Model/DailyTemperature.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherForecastAPI.Model
+{
+    public class DailyTemperature
+    {
+        public double day { get; set; } //day temperature
+        public double min { get; set; } //min daily temperature
+        public double max { get; set; } //max daily temperature
+        public double night { get; set; } //night temperature
+        public double eve { get; set; } //evening temperature
+        public double morn { get; set; } //morning temperature
+    }
+}
