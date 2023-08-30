b18ed16de876f29939a5035c108034f3acc1040b
WK0713 <tan_kee1020@hotmail.com>
2023-08-31 01:16:31 +0800

    Add "OneCallApiResponse.cs" file

---
+++ WeatherForecastAPI/Model/OneCallApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecastAPI.Model
+{
+    public class OneCallApiResponse
+    {
+        public double lat { get; set; } //Latitude of the location, decimal (−90; 90)
+        public double lon { get; set; } //Longitude of the location, decimal (-180; 180)
+        public string timezone { get; set; } //Timezone name for the requested location
+        public int timezone_offset { get; set; } //Shift in seconds from UTC
+        public List<DailyForecast> daily { get; set; } //Daily forecast weather data API response
+    }
+}
